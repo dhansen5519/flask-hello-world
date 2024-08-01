8eec65e62e516b18f1af3900b9be6c6a7d690e91
dhansen5519 <daha2377@colorado.edu>
2024-07-31 18:23:16 -0600

    Implement Database Connection

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
+import psycopg2
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return "Hello World from Darby Hansen in 3308"
+
+@app.route('/db_test')
+def testing():
+    conn = psycopg2.connect("postgresql://lab_10_dq2c_user:O2VsF0jbZhZceKGnbkVqYC4cxSCKLnup@dpg-cqld8irv2p9s73am406g-a/lab_10_dq2c")
+    conn.close()
+    return "Database connection successful."
